fa93793b8cec291b83d5720944ed07f29869f977
David Ellis <davidkellis@gmail.com>
2010-10-29 18:44:19 -0500

    Updated redismodel and added unit tests to test redismodel.

--- example/model.rb
+++ example/model.rb
@@ -2,6 +2,7 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib")
 
 require 'redismodel'
 require 'pp'
+require 'test/unit'
 
 class Address
   include RedisModel
@@ -23,35 +24,71 @@ class Person
   include RedisModel
   
   has_many :addresses, 'Address'
+  
+  attr_accessor :full_name
 end
 
-def main
-  r = Redis.new
+class TestRedisModel < Test::Unit::TestCase
+  def test_redis_model
+    r = Redis.new
   
-  a = Address.create(r, name: 'David', address1: '123 Main', city: 'San Antonio', state: 'TX', years_at_address: 0)
-  a.increment(:years_at_address)
-  pp a
+    a = Address.create(r, name: 'David', address1: '123 Main', city: 'San Antonio', state: 'TX', years_at_address: 0)
+    a.increment(:years_at_address)
+    assert_equal(1, a.years_at_address)
   
-  b = Address.load(r, a.id)
-  pp b
+    b = Address.load(r, a.id)
+    assert_equal('David', b.name)
+    assert_equal('123 Main', b.address1)
+    assert_nil(b.address2)
+    assert_equal('San Antonio', b.city)
+    assert_equal('TX', b.state)
+    assert_equal('1', b.years_at_address)
+    
+    b.decrement(:years_at_address)
+    assert_equal(0, b.years_at_address)
+    
+    a.reload!([:years_at_address])
+    assert_equal('0', a.years_at_address)
+    
+    p = Person.create(r, full_name: "David Ellis")
+    assert_equal('David Ellis', p.full_name)
   
-  p = Person.create(r, name: "David")
-  pp p
-  
-  c = Address.create(r, name: "Ellis1", person: p)
-  c2 = Address.create(r, name: "Ellis2", person: p)
-  pp c
-  pp c2
-  
-  c3 = Address.create(r, name: "Ellis3", person: p)
-  c3.person = nil     # TODO: change this so that a save! is required for the relation to be broken. As is, save! isn't required.
-  
-  d = Address.load(r, c.id)
-  pp d  
-  puts "p.id == c.person.id => #{p.id == c.person.id}"
-  puts "p.id == d.person.id => #{p.id == d.person.id}"
-  
-  pp p.addresses
-end
+    c = Address.create(r, name: "Ellis1", person: p, zip: 123)
+    c2 = Address.create(r, name: "Ellis2", person: p, zip: 123)
+    assert_equal('Ellis1', c.name)
+    assert_equal('Ellis2', c2.name)
+    assert_equal(p.id, c.person_id)
+    assert_equal(p.id, c.person.id)
+    assert_equal(p.id, c2.person_id)
+    assert_equal(p.id, c2.person.id)
+    assert_equal(p.full_name, c.person.full_name)
+    assert_equal(2, p.addresses.length)
 
-main
+    c3 = Address.create(r, name: "Ellis3", person: p, zip: 123)
+    assert_equal('Ellis3', c3.name)
+    assert_equal(p.id, c3.person_id)
+    assert_equal(p.id, c3.person.id)
+    assert_equal(p.full_name, c3.person.full_name)
+    assert_nil(c3.address1)
+
+    assert_equal(3, p.addresses(true).length)
+    assert_equal(['Ellis1', 'Ellis2', 'Ellis3'], p.addresses.map{|a| a.name}.sort)
+    
+    c3.person = nil
+    c3.save!
+    
+    assert_equal(2, p.addresses(true).length)
+    assert_nil(c3.person)
+    assert_equal(['Ellis1', 'Ellis2'], p.addresses.map{|a| a.name}.sort)
+    
+    d = Address.load(r, c.id)
+    assert_equal(c.id, d.id)
+    assert_equal(c.name, d.name)
+    assert_equal("Ellis1", d.name)
+    assert_equal(p.id, c.person_id)
+    assert_equal(p.id, d.person_id)
+    assert_equal(p.id, c.person.id)
+    assert_equal(p.id, d.person.id)
+    assert_equal(c.name, d.name)
+  end
+end
--- lib/redismodel.rb
+++ lib/redismodel.rb
@@ -1,7 +1,10 @@
+require 'set'
 require 'redis'
 require 'uuidtools'
 require 'moduletemplate'
 
+# Array.hashify([2, 3, 4]) {|i| 2*i }
+# -> {2 => 4, 3 => 6, 4 => 8}
 def Array.hashify(arr, &blk)
   arr.reduce(Hash.new) do |memo, item|
     memo[item] = blk.call(item)
@@ -11,6 +14,17 @@ end
 
 module RedisModel
   module ModuleTemplates
+    # params: field_name
+    FieldGetterAndSetter = ModuleTemplate.new(<<-'MOD')
+      def !{field_name}()
+        @!{field_name}
+      end
+      
+      def !{field_name}=(value)
+        changed_fields << :!{field_name}
+        @!{field_name} = value
+      end
+    MOD
     
     # params: relation_name, other_model_class_name
     # define finder method and getter method
@@ -39,17 +53,22 @@ module RedisModel
         end
       end
       
+      def !{relation_name}_id_last_saved()
+        @!{relation_name}_id_last_saved
+      end
+      
+      def !{relation_name}_id_last_saved=(value)
+        @!{relation_name}_id_last_saved = value
+      end
+      
       def !{relation_name}
         return @!{relation_name} if @!{relation_name} && @!{relation_name}.id == !{relation_name}_id
-        @!{relation_name} = !{other_model_class_name}.load(redis, !{relation_name}_id)
+        @!{relation_name} = !{relation_name}_id ? !{other_model_class_name}.load(redis, !{relation_name}_id) : nil
       end
       
       def !{relation_name}=(model_obj)
-        unregister_from_has_many_relation_on("!{other_model_class_name}", self.!{relation_name}_id, "!{reverse_relation}")
-        
         @!{relation_name} = model_obj
         self.!{relation_name}_id = model_obj ? model_obj.id : nil
-        save!([:!{relation_name}_id])
         @!{relation_name}
       end
     MOD
@@ -60,7 +79,7 @@ module RedisModel
       fields << name
       
       self.module_eval do
-        attr_accessor name.to_sym
+        include ModuleTemplates::FieldGetterAndSetter.generate(field_name: name.to_s)
       end
     end
     
@@ -70,7 +89,7 @@ module RedisModel
       # So, for now, model inheritance is not supported (i.e. one model can't inherit from another
       # with the expectation that the fields from the superclass will transfer to the subclass model).
       # @fields ||= superclass.respond_to?(:fields) ? superclass.fields + [:id] : [:id]
-      @fields ||= [:id]
+      @fields ||= Set.new([:id])
     end
     
     # This method will read a redis set with a key-name of "<this model class name>:id:relation".
@@ -86,7 +105,7 @@ module RedisModel
     end
     
     def has_many_relations
-      @has_many_relations ||= []
+      @has_many_relations ||= Set.new
     end
     
     # Example:
@@ -101,7 +120,16 @@ module RedisModel
     end
     
     def belongs_to_relations
-      @belongs_to_relations ||= []
+      @belongs_to_relations ||= Set.new
+    end
+    
+    def belongs_to_relation_names
+      if @last_belongs_to_relations == belongs_to_relations
+        @cached_relation_names
+      else
+        @last_belongs_to_relations = belongs_to_relations
+        @cached_relation_names = @last_belongs_to_relations.map {|tuple| tuple[0] }.to_set
+      end
     end
     
     def create(redis, attrs = {})
@@ -115,7 +143,6 @@ module RedisModel
     
     def load(redis, id, attrs = [])
       m = self.new(id: id) { |obj| obj.redis = redis }
-      m.redis = redis
       attrs = fields if attrs.count == 0
       m.reload!(attrs)
       m
@@ -135,6 +162,26 @@ module RedisModel
     do_assignments_to_self(attrs)
   end
   
+  def set_field(field_name, value)
+    instance_variable_set("@#{field_name}".to_sym, value)
+  end
+  
+  def changed_fields()
+    @changed_fields ||= Set.new
+  end
+  
+  # Use this method to unflag a field or set of fields as "changed".
+  # The set of changed_fields is used by the save! method.
+  # This will remove all the fields from the changed_fields set if fields = nil
+  # otherwise, only the fields specified in the fields array/set will be unflagged as 'changed'.
+  def unflag_changed_fields(fields = nil)
+    if fields
+      @changed_fields = changed_fields - fields.to_set
+    else
+      @changed_fields = Set.new
+    end
+  end
+
   # takes a hash and for each key/value pair, does one of the following:
   # 1. If <key>= is a defined method, then the following assignment is made
   #    self.<key> = value
@@ -142,7 +189,7 @@ module RedisModel
   def do_assignments_to_self(attrs = {})
     attrs.each do |k, v|
       method = "#{k}=".to_sym
-      if self.respond_to? method
+      if !self.class.fields.include?(k) && self.respond_to?(method)
         self.send(method, v)
       else
         instance_variable_set("@#{k}".to_sym, v)
@@ -151,27 +198,31 @@ module RedisModel
   end
   
   def increment(att, increment_amt = 1)
-    if self.class.fields.include?(att)
-      redis.hincrby(key_prefix, att, increment_amt)
-    else
-      if increment_amt == 1
-        redis.incr(key_for(att))
-      else
-        redis.incrby(key_for(att), increment_amt)
-      end
-    end
+    new_value = if self.class.fields.include?(att)
+                  redis.hincrby(key_prefix, att, increment_amt)
+                else
+                  if increment_amt == 1
+                    redis.incr(key_for(att))
+                  else
+                    redis.incrby(key_for(att), increment_amt)
+                  end
+                end
+    do_assignments_to_self(att => new_value)
+    new_value
   end
   
   def decrement(att, decrement_amt = 1)
-    if self.class.fields.include?(att)
-      redis.hincrby(key_prefix, att, -decrement_amt)
-    else
-      if decrement_amt == 1
-        redis.decr(key_for(att))
-      else
-        redis.decrby(key_for(att), decrement_amt)
-      end
-    end
+    new_value = if self.class.fields.include?(att)
+                  redis.hincrby(key_prefix, att, -decrement_amt)
+                else
+                  if decrement_amt == 1
+                    redis.decr(key_for(att))
+                  else
+                    redis.decrby(key_for(att), decrement_amt)
+                  end
+                end
+    do_assignments_to_self(att => new_value)
+    new_value
   end
   
   def key_prefix
@@ -216,6 +267,7 @@ module RedisModel
   # get_remote_attributes(:b, :a)
   # => {:b => "0", :a => "23"}
   def get_remote_attributes(attributes = [])
+    attributes = attributes.to_a
     return {} if attributes.count == 0
     values = redis.hmget(key_prefix, *attributes)
     Hash[attributes.zip(values)]
@@ -238,12 +290,31 @@ module RedisModel
   end
   
   def reload!(attrs = [])
-    # do_assignments_to_self(Array.hashify(attrs) {|attribute| get_remote_attribute(attribute)})
     do_assignments_to_self(unmarshal_attributes_hash(get_remote_attributes(attrs)))
+    
+    reload_last_saved_ids_for_belongs_to_relations(attrs)
   end
   
   def reload_all!
     reload!(self.class.fields)
+  end
+  
+  # Takes a list of field names. If any of the field names represent a field id used in a belongs_to relation
+  # then the current value of the id field (e.g. address_id) is used to represent the last-saved (i.e. last known)
+  # id of the model object to which this model object belongs.
+  def reload_last_saved_ids_for_belongs_to_relations(field_names)
+    # figure out which of the field_names are belongs_to relation model ids
+    reloaded_belongs_to_relation_ids = field_names.to_set & self.class.belongs_to_relation_names.map{|name_sym| "#{name_sym}_id".to_sym }.to_set
+    # for each relation model id field, update the last saved model object id with the currently referenced relation model id
+    reloaded_belongs_to_relation_ids.each do |field_id_sym|
+      update_last_saved_belongs_to_model_id(field_id_sym)
+    end
+  end
+  
+  # relation_name_id_field is as symbl of the form:  :<relation name>_id
+  def update_last_saved_belongs_to_model_id(relation_name_id_field, current_id = nil)
+    current_id ||= self.send(relation_name_id_field)
+    self.send("#{relation_name_id_field}_last_saved=", current_id)
   end
   
   def marshal(attribute)
@@ -275,20 +346,49 @@ module RedisModel
   # with a key-name of "<relation model class name>:id:relation"
   def register_in_has_many_relation_on(local_relation_name, other_model_class_name, other_model_has_many_relation)
     # redis.sadd(other_model_object.key_for_has_many_relation(other_model_has_many_relation), self.id)
-    model_id = self.send("#{local_relation_name}_id".to_sym)
+    local_relation_name_sym = "#{local_relation_name}_id".to_sym
+    model_id = self.send(local_relation_name_sym)
     if(model_id)
       redis.sadd(key_for_has_many_relation_on_other_model(other_model_class_name, model_id, other_model_has_many_relation), self.id)
+      update_last_saved_belongs_to_model_id(local_relation_name_sym, model_id)
     end
   end
   
-  def unregister_from_has_many_relation_on(other_model_class_name, model_id, other_model_has_many_relation)
-    if(model_id)
-      redis.srem(key_for_has_many_relation_on_other_model(other_model_class_name, model_id, other_model_has_many_relation), self.id)
+  # remove this model object's id to the redis set
+  # with a key-name of "<relation model class name>:id:relation"
+  def unregister_from_has_many_relation_on(local_relation_name, other_model_class_name, other_model_has_many_relation)
+    current_related_model_id = self.send("#{local_relation_name}_id".to_sym)
+    last_saved_model_id = self.send("#{local_relation_name}_id_last_saved".to_sym)
+    if(last_saved_model_id && last_saved_model_id != current_related_model_id)
+      redis.srem(key_for_has_many_relation_on_other_model(other_model_class_name, last_saved_model_id, other_model_has_many_relation), self.id)
     end
   end
   
-  def save!(attrs = [])
-    attrs.each do |attribute|
+  # takes a set of field names and determines if any of them are belongs_to relation names
+  # of the field names that are belongs_to relation_names, it converts the field names to
+  # the corresponding id field names
+  # For example, if :address and :account are belongs_to relation names, then
+  # identify_and_convert_id_field_names(Set.new([:name, :address, :account, :age]))
+  # -> #<Set: {:address_id, :account_id}>
+  # Returns a set.
+  def identify_and_convert_id_field_names(field_names)
+    # identify which field_names are also belongs_to relation names
+    field_names = (self.class.belongs_to_relation_names & field_names)
+    field_names.map {|field_name| "#{field_name}_id".to_sym }.to_set
+  end
+  
+  # Accepts an array of field names
+  def save!(attrs = nil)
+    attrs ||= changed_fields
+    
+    attrs = attrs.to_set
+    
+    attrs = attrs + identify_and_convert_id_field_names(attrs)
+    
+    unflag_changed_fields(attrs)
+    
+    # we only want to call set_remote_attribute for attributes that were defined with the 'field' method/"macro".
+    (attrs & self.class.fields).each do |attribute|
       set_remote_attribute(attribute, marshal(attribute))
     end
     
@@ -300,7 +400,12 @@ module RedisModel
       reverse_relation_class_name = relation_3_tuple[1]   # second item in the tuple is the class name of the model which has the has_many relation with this model class
       reverse_relation_name = relation_3_tuple[2]         # the third item in the tuple is the relation name on the other model
       
-      register_in_has_many_relation_on(local_relation_name, reverse_relation_class_name, reverse_relation_name)
+      local_relation_name_sym = "#{local_relation_name}_id".to_sym
+      
+      # if attrs.include?(local_relation_name_sym)
+        unregister_from_has_many_relation_on(local_relation_name, reverse_relation_class_name, reverse_relation_name)
+        register_in_has_many_relation_on(local_relation_name, reverse_relation_class_name, reverse_relation_name)
+      # end
     end
   end
   
